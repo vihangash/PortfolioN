82b96aa3adcab58b294338b409ea92a25852fb7e
Vihanga Yapa <47640992+vihangash@users.noreply.github.com>
2024-10-14 18:40:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,34 @@ menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 // Script for scroll-based animations
+   // Script for scroll-based animations
     window.addEventListener('scroll', () => {
+        const heroSection = document.querySelector('.hero-section');
         const aboutSection = document.querySelector('.about-section');
         const workSection = document.querySelector('.work-section');
-        const aboutLink = document.querySelector(".nav-links a[href='#about']");        
+        const contactSection = document.querySelector('.contact-section');
+        const homeLink = document.querySelector(".nav-links a[href='#home']");
+        const aboutLink = document.querySelector(".nav-links a[href='#about']");
         const workLink = document.querySelector(".nav-links a[href='#work']");
+        const contactLink = document.querySelector(".nav-links a[href='#contact']");
 
-        
+        const heroSectionPosition = heroSection.getBoundingClientRect().top;
         const aboutSectionPosition = aboutSection.getBoundingClientRect().top;
         const workSectionPosition = workSection.getBoundingClientRect().top;
+        const contactSectionPosition = contactSection.getBoundingClientRect().top;
         const screenPosition = window.innerHeight / 1.3;
 
+        if (heroSectionPosition < screenPosition && heroSectionPosition > -screenPosition) {
+            homeLink.classList.add('active');
+        } else {
+            homeLink.classList.remove('active');
+        }
 
         if (aboutSectionPosition < screenPosition) {
             aboutSection.classList.add('visible');
             aboutLink.classList.add('active');
             workLink.classList.remove('active');
+            contactLink.classList.remove('active');
         } else {
             aboutLink.classList.remove('active');
         }
@@ -30,7 +42,17 @@ menuIcon.addEventListener('click', () => {
             workSection.classList.add('visible');
             workLink.classList.add('active');
             aboutLink.classList.remove('active');
+            contactLink.classList.remove('active');
         } else {
             workLink.classList.remove('active');
+        }
+
+        if (contactSectionPosition < screenPosition) {
+            contactSection.classList.add('visible');
+            contactLink.classList.add('active');
+            workLink.classList.remove('active');
+            aboutLink.classList.remove('active');
+        } else {
+            contactLink.classList.remove('active');
         }
     });
