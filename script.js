c8e613fe3071a57a63890cab8b63f53683cae54b
Vihanga Yapa <47640992+vihangash@users.noreply.github.com>
2024-10-14 13:58:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,3 @@ const navLinks = document.getElementById('nav-links');
 menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
-
-// Script for scroll-based animations
-window.addEventListener('scroll', () => {
-    const aboutSection = document.querySelector('.about-section');
-    const aboutLink = document.querySelector(".nav-links a[href='#about']");
-    const sectionPosition = aboutSection.getBoundingClientRect().top;
-    const screenPosition = window.innerHeight / 1.3;
-
-    if (sectionPosition < screenPosition) {
-        aboutSection.classList.add('visible');
-        aboutLink.classList.add('active');
-    } else {
-        aboutLink.classList.remove('active');
-    }
-});
