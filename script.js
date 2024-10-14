3595a89bf73f662ba69c7caf8930821be81ce65d
Vihanga Yapa <47640992+vihangash@users.noreply.github.com>
2024-10-14 14:04:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ const navLinks = document.getElementById('nav-links');
 menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+window.addEventListener('scroll', () => {
+        const aboutSection = document.querySelector('.about-section');
+        const aboutLink = document.querySelector(".nav-links a[href='#about']");
+        const sectionPosition = aboutSection.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.3;
+
+        if (sectionPosition < screenPosition) {
+            aboutSection.classList.add('visible');
+            aboutLink.classList.add('active');
+        } else {
+            aboutLink.classList.remove('active');
+        }
+    });
